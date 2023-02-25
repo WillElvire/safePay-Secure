c3441c5909b719ada7a60f6a1668c69cb6eefb90
macbook <wkoua@oxylead.net>
2023-02-25 07:37:30 +0000

    feat : initialisation

---
+++ src/app/modules/layouts.module.ts
@@ -0,0 +1,11 @@
+import { AdminLayoutComponent } from './../layouts/admin-layout/admin-layout.component';
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  declarations  : [AdminLayoutComponent],
+  imports : [],
+  exports: [AdminLayoutComponent]
+})
+export class LayoutModule {
+
+}
