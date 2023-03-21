9ee0eaff467fb328c6333d0721b878f1affd8661
macbook <wkoua@oxylead.net>
2023-03-21 15:20:48 +0000

    feat : [ok] to deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production : false,
+  production : true,
   BASE_URL : 'http://localhost:8080/',
   ADACIKEY :'adaciUser',
   COINAPIKEY : "CA1607AF-4043-4D3E-BC9D-38D98FEF576C",
